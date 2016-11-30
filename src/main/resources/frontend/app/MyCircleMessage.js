94c0e0aab82ad84089e587b2fe1aca3105acab48
Brian Sandler <brianmsandler@gmail.com>
2016-11-30 13:35:11 -0800

    Adding delete to mycircle.

--- src/main/resources/frontend/app/MyCircleMessage.js
+++ src/main/resources/frontend/app/MyCircleMessage.js
@@ -1,12 +1,52 @@
 import React from 'react'
+import $ from 'jquery'
+import Store from './Store'
 
 export default class extends React.Component {
+    
+    constructor() {
+        super();
+        this.state = {
+            showDelete: false
+        };
+    }
 
     getTopics() {
         return this.props.topics.map((topic, idx) => {
             return (<span key={idx} className="label label-default">{topic}</span>);
         });
     }
+    
+    componentDidMount() {
+        new Store().getUser((user) => {
+            if (user.userid === this.props.userid) {
+                this.setState({
+                    showDelete: true
+                });
+            }
+        });
+    }
+    
+    deleteMessage() {
+        new Store().getAuth((auth) => {
+            console.log("Deleting message.");
+            $.ajax({
+                method: "POST",
+                url: `http://localhost:8080/api/mycircle/delete?mid=${this.props.mid}`,
+                beforeSend: function (request)
+                {
+                    request.setRequestHeader("auth_token", auth);
+                }
+            })
+                .done(() => {
+                    alert("Message Deleted!");
+                })
+                .fail(function (err) {
+                    alert("Could not delete message: " + JSON.stringify(err));
+                });
+        });
+    }
+    
     render() {
         return (
         <div className="media mycircle-message">
@@ -19,7 +59,8 @@ export default class extends React.Component {
                 }
             </div>
             <div className="media-body">
-                <h4 className="media-heading">{this.props.screenname}</h4>
+                <h4 className="media-heading">{this.props.screenname} {this.state.showDelete ? <button className="delete-button" onClick={this.deleteMessage.bind(this)}><span
+                    className="glyphicon glyphicon-remove-circle"/></button> : "" }</h4>
                 <p><span className="time">{this.props.utc}</span> - {this.props.readCount} views</p>
                 <p className="media-body">{this.props.msg}</p>
                 <p className="topics">
